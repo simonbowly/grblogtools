c261d4e56522175f418d21664609e1acf82f638f
Maliheh Aramon <maliheha@gmail.com>
2022-01-06 15:25:21 -0800

    simplify the simplex parser

--- src/grblogtools/simplex_parser.py
+++ src/grblogtools/simplex_parser.py
@@ -18,25 +18,12 @@ class SimplexParser:
     # solving an MIP. In some cases, the simplex log might only include this one
     # line
     simplex_mip_relaxation_pattern = re.compile(
-        r"Root relaxation: objective (?P<ContinuousObjective>[^,]+), (?P<ContinuousIteration>\d+) iterations, (?P<ContinuousTime>[^\s]+) seconds"
-    )
-    # The patterns indicating the termination of the simplex in case of solving an LP.
-    # The LP refers to all continuous optimization problems. The first pattern
-    # always exists but the second pattern might not exist
-    simplex_lp_iteraion_time_termination_pattern = re.compile(
-        r"(Solved|Stopped) in (?P<ContinuousIteration>[^\s]+) iterations and (?P<ContinuousTime>[^\s]+) seconds"
-    )
-    simplex_lp_obj_termination_pattern = re.compile(
-        r"Optimal objective\s+(?P<ContinuousObjective>.*)$"
+        r"Root relaxation: objective (?P<RelaxObj>[^,]+), (?P<RelaxIterCount>\d+) iterations, (?P<RelaxTime>[^\s]+) seconds"
     )
 
     def __init__(self):
         """Initialize the Simplex parser."""
-        self._summary = {
-            "ContinuousObjective": None,
-            "ContinuousIteration": None,
-            "ContinuousTime": None,
-        }
+        self._summary = {}
         self._progress = []
 
     def start_parsing(self, line: str) -> bool:
@@ -66,9 +53,7 @@ class SimplexParser:
 
         Returns:
             bool: Return True if the line matches the pattern indicating progress in
-                the simplex method. Return True for the termination patterns except for
-                the ones which certainly point to the full termination of the simplex
-                method.
+                the simplex method.
         """
         progress_match = SimplexParser.simplex_progress_pattern.match(line)
         if progress_match:
@@ -80,36 +65,15 @@ class SimplexParser:
             self._summary.update(typeconvert_groupdict(mip_relaxation_match))
             return False
 
-        lp_iteration_time_termination_match = (
-            SimplexParser.simplex_lp_iteraion_time_termination_pattern.match(line)
-        )
-        if lp_iteration_time_termination_match:
-            parsed_dict = typeconvert_groupdict(lp_iteration_time_termination_match)
-            self._summary["ContinuousIteration"] = parsed_dict["ContinuousIteration"]
-            self._summary["ContinuousTime"] = parsed_dict["ContinuousTime"]
-            return True
-
-        lp_obj_termination_match = (
-            SimplexParser.simplex_lp_obj_termination_pattern.match(line)
-        )
-        if lp_obj_termination_match:
-            parsed_dict = typeconvert_groupdict(lp_obj_termination_match)
-            self._summary["ContinuousObjective"] = parsed_dict["ContinuousObjective"]
-            return False
-
         return False
 
     def get_summary(self) -> dict:
         """Return the current parsed summary.
 
-        It returns an empty dictionary if the parser is not initialized yet.
+        It returns an empty dictionary when solving an LP.
         """
         return self._summary
 
-    def get_progress(self) -> dict:
-        """Return the detailed progress in simplex method.
-
-        It returns an empty list if the parser is not initialized yet or there is
-        no progress to report.
-        """
+    def get_progress(self) -> list:
+        """Return the detailed progress in simplex method if exists."""
         return self._progress
