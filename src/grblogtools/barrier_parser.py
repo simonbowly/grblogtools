1dfc528242cf541948c10ea41e7c3c97470ee56d
Maliheh Aramon <maliheha@gmail.com>
2022-01-09 23:55:03 -0800

    add the barrier parser

--- src/grblogtools/barrier_parser.py
+++ src/grblogtools/barrier_parser.py
@@ -14,21 +14,22 @@ class BarrierParser:
         r"\s*(?P<Iteration>\d+)(?P<Indicator>\s|\*)\s+(?P<PObj>[^\s]+)\s+(?P<DObj>[^\s]+)\s+(?P<PRes>[^\s]+)\s+(?P<DRes>[^\s]+)\s+(?P<Compl>[^\s]+)\s+(?P<Time>\d+)s"
     )
 
-    # The pattern indicating the start or the termination of the barrier in case of
-    # solving an MIP. In some cases, the barrier log might only include this one
-    # line
-    barrier_mip_relaxation_pattern = re.compile(
-        r"Root relaxation: objective (?P<RelaxObj>[^,]+), (?P<RelaxIterCount>\d+) iterations, (?P<RelaxTime>[^\s]+) seconds"
+    barrier_crossover_pattern = re.compile(
+        r"\s*Push phase complete: Pinf (?P<PushPhasePInf>[^\s]+), Dinf (?P<PushPhaseDInf>[^\s]+)\s+(?P<PushPhaseEndTime>\d+)s"
     )
 
-    barrier_crossover_pattern = re.compile(
-        r"Push phase complete: Pinf (?P<PushPhasePInf>[^,]+), Dinf (?P<PushPhaseDInf>[^,]+)\s+(?P<PushPhaseEndTime>\d+)s"
-    )
+    barrier_termination_patterns = [
+        re.compile(
+            r"Barrier solved model in (?P<BarIterCount>[^\s]+) iterations and (?P<Runtime>[^\s]+) seconds"
+        ),
+        re.compile(
+            r"Barrier performed (?P<BarIterCount>\d+) iterations in (?P<Runtime>[^\s]+) seconds"
+        ),
+    ]
 
     def __init__(self):
         """Initialize the Barrier parser."""
         self._summary = {}
-        self._crossover = {}
         self._progress = []
 
     def start_parsing(self, line: str) -> bool:
@@ -42,12 +43,6 @@ class BarrierParser:
         """
         if BarrierParser.barrier_start_pattern.match(line):
             return True
-
-        mip_relaxation_match = BarrierParser.barrier_mip_relaxation_pattern.match(line)
-        if mip_relaxation_match:
-            self._summary.update(typeconvert_groupdict(mip_relaxation_match))
-            return True
-
         return False
 
     def continue_parsing(self, line: str) -> bool:
@@ -57,36 +52,30 @@ class BarrierParser:
             line (str): A line in the log file.
 
         Returns:
-            bool: Return True if the line matches the pattern indicating progress in
-                the barrier method.
+            bool: Return True if the parser should continue parsing future log lines.
         """
         progress_match = BarrierParser.barrier_progress_pattern.match(line)
         if progress_match:
             self._progress.append(typeconvert_groupdict(progress_match))
             return True
 
+        for barrier_termination_pattern in BarrierParser.barrier_termination_patterns:
+            barrier_termination_match = barrier_termination_pattern.match(line)
+            if barrier_termination_match:
+                self._summary.update(typeconvert_groupdict(barrier_termination_match))
+                return True
+
         crossover_match = BarrierParser.barrier_crossover_pattern.match(line)
         if crossover_match:
-            self._crossover.update(typeconvert_groupdict(crossover_match))
+            self._summary.update(typeconvert_groupdict(crossover_match))
             return True
-
-        mip_relaxation_match = BarrierParser.barrier_mip_relaxation_pattern.match(line)
-        if mip_relaxation_match:
-            self._summary.update(typeconvert_groupdict(mip_relaxation_match))
 
         return False
 
     def get_summary(self) -> dict:
-        """Return the current parsed summary.
-
-        It returns an empty dictionary when solving an LP.
-        """
+        """Return the current parsed summary."""
         return self._summary
 
     def get_progress(self) -> list:
-        """Return the detailed progress in simplex method if ."""
+        """Return the detailed progress in barrier method."""
         return self._progress
-
-    def get_crossover_summary(self) -> dict:
-        """Return the crossover summary if exists."""
-        return self._crossover
